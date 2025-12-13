caef871b71a7747a0280eda8b03f6fa9c1f9fb77
alho293h <al-amin.hossain@mailbox.tu-dresden.de>
2025-12-13 12:34:44 +0100

    Implementing cnn.py,  edited mlp.py in flatten layer.

--- src/data/cnn.py
+++ src/data/cnn.py
@@ -23,4 +23,12 @@ class MLP_network(nn.Module):
         self.fc2 = nn.Linear(512, 7)
 
     def forward(self, x):
-        pass
+        x=self.pool1(self.relu1(self.conv1(x)))
+        x=self.pool2(self.relu2(self.conv2(x)))
+        x=self.pool3(self.relu3(self.conv3(x)))
+
+        x=torch.flatten(self.drop1(x),1)
+        x=self.drop2(self.relu4(self.fc1(x)))
+
+        x=self.fc2(x)
+        return x
